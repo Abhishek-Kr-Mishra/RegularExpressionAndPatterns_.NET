1fbe1d3747d44b42ecd2ed04dab6b181d6981530
abh224272 <abh224272@gmail.com>
2020-12-21 16:15:46 +0530

    [Abhishek Mishra] Add: Regex To Validate Last Name

--- RegularExpressionAndPatternsProgram/PatternsMatching.cs
+++ RegularExpressionAndPatternsProgram/PatternsMatching.cs
@@ -8,12 +8,19 @@ namespace RegularExpressionAndPatternsProgram
     class PatternsMatching
     {
         public static string REGEX_firstName = "^[A-Z][a-zA-Z]{2,}";
+        public static string REGEX_lastName = "^[A-Z][a-zA-Z]{2,}";
 
         public bool ValidateFirstName()
         {
             Console.WriteLine("Enter the First Name");
             string fname = Console.ReadLine(); ;
             return Regex.IsMatch(fname, REGEX_firstName);
+        }
+        public bool ValidateLastName()
+        {
+            Console.WriteLine("Enter the Last Name");
+            string lname = Console.ReadLine(); ;
+            return Regex.IsMatch(lname, REGEX_lastName);
         }
     }
 }
--- RegularExpressionAndPatternsProgram/Program.cs
+++ RegularExpressionAndPatternsProgram/Program.cs
@@ -9,8 +9,11 @@ namespace RegularExpressionAndPatternsProgram
             Console.WriteLine("Welcome To User Registration Regex And Pattern Matching");
             PatternsMatching patternsMatching = new PatternsMatching();
 
-            if (patternsMatching.ValidateFirstName()){ Console.WriteLine("Correct First Name"); }
+            if (patternsMatching.ValidateFirstName()){ Console.WriteLine("Correct First Name Format"); }
             else { Console.WriteLine("First Name format is wrong"); }
+
+            if (patternsMatching.ValidateFirstName()) { Console.WriteLine("Correct Last Name Format"); }
+            else { Console.WriteLine("Last Name format is wrong"); }
         }
     }
 }
