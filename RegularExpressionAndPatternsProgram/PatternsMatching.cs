601f049e74950ea93d1cb294fab0a67837ec55d8
abh224272 <abh224272@gmail.com>
2020-12-21 16:06:51 +0530

    [Abhishek Mishra] Add: Regex to Validate First Name

---
+++ RegularExpressionAndPatternsProgram/PatternsMatching.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionAndPatternsProgram
+{
+    class PatternsMatching
+    {
+        public static string REGEX_firstName = "^[A-Z][a-zA-Z]{2,}";
+
+        public bool ValidateFirstName()
+        {
+            Console.WriteLine("Enter the First Name");
+            string fname = Console.ReadLine(); ;
+            return Regex.IsMatch(fname, REGEX_firstName);
+        }
+    }
+}
--- RegularExpressionAndPatternsProgram/Program.cs
+++ RegularExpressionAndPatternsProgram/Program.cs
@@ -7,6 +7,10 @@ namespace RegularExpressionAndPatternsProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To User Registration Regex And Pattern Matching");
+            PatternsMatching patternsMatching = new PatternsMatching();
+
+            if (patternsMatching.ValidateFirstName()){ Console.WriteLine("Correct First Name"); }
+            else { Console.WriteLine("First Name format is wrong"); }
         }
     }
 }
