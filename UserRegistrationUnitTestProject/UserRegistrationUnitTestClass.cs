02a45d45fdba4741c02cf6b7b6371790ab8cc08b
abh224272 <abh224272@gmail.com>
2021-01-05 10:59:11 +0530

    [Abhishek Mishra] Add: Parameterised Unit test to validate multiple Email Address

--- UserRegistrationUnitTestProject/UserRegistrationUnitTestClass.cs
+++ UserRegistrationUnitTestProject/UserRegistrationUnitTestClass.cs
@@ -8,29 +8,41 @@ namespace UserRegistrationUnitTestProject
     {
         readonly PatternsMatching patternsMatching = new PatternsMatching();
         [TestMethod]
-        public void ValidateFirsNameTestMethod()
+        public void GivenFirstName_WhenValidate_ShouldReturnTrue()
         {
             Assert.AreEqual(true, patternsMatching.ValidateFirstName("Abhishek"));
         }
         [TestMethod]
-        public void ValidateLastNameTestMethod()
+        public void GivenLastName_WhenValidate_ShouldReturnTrue()
         {
             Assert.AreEqual(true, patternsMatching.ValidateLastName("Mishra"));
         }
         [TestMethod]
-        public void ValidateEmailTestMethod()
+        public void GivenEmail_WhenValidate_ShouldReturnTrue()
         {
             Assert.AreEqual(true, patternsMatching.ValidateEmail("abh224272@gmail.com"));
         }
         [TestMethod]
-        public void ValidatePasswordTestMethod()
+        public void GivenPassword_WhenValidate_ShouldReturnTrue()
         {
             Assert.AreEqual(true, patternsMatching.ValidatePassword("abhishekMishrA@124"));
         }
         [TestMethod]
-        public void ValidatePhoneNumber()
+        public void GivenPhoneNumber_WhenValidate_ShouldReturnTrue()
         {
             Assert.AreEqual(true, patternsMatching.ValidatePhoneNumber("+917559171697"));
+        }
+        [DataRow("1.abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataTestMethod]
+        public void GivenMultipleEntryOfEmailAddress_WhenValidate_ShouldReturnTrue(string email)
+        {
+            Assert.AreEqual(true, patternsMatching.ValidateEmail(email));
         }
     }
 }
